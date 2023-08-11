f0c809da8df1a4f7005f422c33856d55c8b05f4a
Sharareh Golshani <shararehg79@yahoo.com>
2023-08-10 21:06:06 -0700

    updated js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,65 @@ var viewedImagesList = [];
 var verse = [];
 var image = [];
 
+var pixaKeyS = "38768763-bb697d80fc015bc0e4d2af0a5";
+// var pixaKey = "38708546-2cfb7c47338a280cf45ea2b47";
+var bibleKey = "e0069271426c93138b3100997ccfbd51";
+
 
 // function to fetch, display and save verses. 
+
 var displayVerse = function () {
-  fetch("bible api here")
-    .then(response => response.json())
-    .then(data => (versePlacement.innerHTML = data.verse));
+    fetch("bible api here")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      versePlacement.innerHTML = data.verse;
+      viewedVersesList.push(displayVerse.text);
+      localStorage.setItem("viewedVersesList", JSON.stringify(viewedVersesList));
 
-  viewedVersesList.push(displayVerse.text);
-  localStorage.setItem("viewedVersesList", JSON.stringify(viewedVersesList));
-  displayVerse();
-}
+      displayVerse();
+});
 
+};
 // function to fetch, save and display verses and Images
 // maybe create a save button and only save those user chooses to save? 
 // check to make sure this doesn't create double inputs, pretty sure
 // need to add condition to verse display that if verse id doesn't exist then 
 // save.  maybe should do that anyway. 
-var displayImage = function () {
-  fetch("image api here")
-    .then(response => response.json())
-    .then(data => (imagePlacement.innerHTML = data.image));
-  imagePlacement.style.backgroundImage = data.image;
 
-  viewedImagesList.push(displayImage.pic);
-  localStorage.setItem("viewedImagesList", JSON.stringify(viewedImagesList));
-  displayImage();
 
-  fetch("bible api here")
-    .then(response => response.json())
-    .then(data => (versePlacement.innerHTML = data.verse));
+    
 
-  viewedVersesList.push(displayVerse.text);
-  localStorage.setItem("viewedVersesList", JSON.stringify(viewedVersesList));
-  displayVerse();
-}
+function displayImage () {
+  fetch(`https://pixabay.com/api/?key=${pixaKeyS}`)
+  .then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+  console.log('data', image);
+
+    // imagePlacement.style.backgroundImage = data.image;
+    // viewedImagesList.push(displayImage.pic);
+    // localStorage.setItem("viewedImagesList", JSON.stringify(viewedImagesList));
+
+    // .catch(error => console.error('Error:', error));
+
+    // displayImage();
+    
+  });
+
+fetch("bible api here")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    versePlacement.innerHTML = data.verse;
+    viewedVersesList.push(displayVerse.text);
+    localStorage.setItem("viewedVersesList", JSON.stringify(viewedVersesList));
+    displayVerse();
+    });  
+};
 
 // funciton to display the previous searches on the page. 
 // Need to limit how many are saved in localstorage. Figure out how to do that.
@@ -53,8 +77,8 @@ var showSearches = function () {
     viewedVerses = viewedVersesList;
     for (i = 0; i < searchedCitiesList.length; i++) {
       var viewedVerses = document.getElementById("viewedVersesList");
-      viewedVerses.innerHTML = "<li>" + viewedVerses + "</li>"; 
-        
+      viewedVerses.innerHTML = "<li>" + viewedVerses + "</li>";
+
     }
   }
   var viewedImagesList = JSON.parse(localStorage.getItem("viewedImagesList"));
@@ -62,8 +86,8 @@ var showSearches = function () {
     viewedImages = viewedImagesList;
     for (i = 0; i < viewedImagesList.length; i++) {
       var viewedImages = document.getElementById("viewedImagesList");
-      viewedImages.innerHTML = "<li>" + viewedImages + "</li>"; 
-        
+      viewedImages.innerHTML = "<li>" + viewedImages + "</li>";
+
     }
   }
 };
@@ -71,3 +95,4 @@ var showSearches = function () {
 // event listeners for user action on page 
 verseButton.addEventListener("click", displayVerse);
 imageButton.addEventListener("click", displayImage);
+};
