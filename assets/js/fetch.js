122d4295a1e0d414dea5529e3fc9c53724d78876
RaptureMe <arosu4u@gmail.com>
2023-08-10 20:59:46 -0700

    Bible API finished, randomized entire bible and cleaned up

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -3,14 +3,15 @@ var bibleKey = "e0069271426c93138b3100997ccfbd51";
 // var verseId = 'JHN.3.16'; // I inserted example chapter w/ verse
 var bibleId = 'de4e12af7f28f599-02'; // This is for the Bible version
 // var chapterId = 'JHN.1' // This is how a chapter ID looks
-var chapterBookId = "JHN"
+// var chapterBookId = "JHN"
 var pixaUrl = `https://pixabay.com/api/?key=${pixaKey}`;
-var allChapters = `https://api.scripture.api.bible/v1/bibles/${bibleId}/books/${chapterBookId}/chapters`; // These two variables are for pulling data in the console log
+var allBooks = 'https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/books';
+// var allChapters = `https://api.scripture.api.bible/v1/bibles/${bibleId}/books/${chapterBookId}/chapters`; // These two variables are for pulling data in the console log
 // var allVerses = `https://api.scripture.api.bible/v1/bibles/${bibleId}/chapters/${chapterId}/verses`; // These two variables are for pulling data in the console log
 // var singleVerse = `https://api.scripture.api.bible/v1/bibles/${bibleId}/verses/${verseId}`; // Pulls a verse
 
 function fetchVerse(verseId) {
-    var singleVerse = `https://api.scripture.api.bible/v1/bibles/${bibleId}/verses/${verseId}`; 
+    var singleVerse = `https://api.scripture.api.bible/v1/bibles/${bibleId}/verses/${verseId}?content-type=text&include-verse-numbers=false`; 
     fetch(singleVerse, {
         headers: {
           'api-key': 'e0069271426c93138b3100997ccfbd51',
@@ -19,11 +20,9 @@ function fetchVerse(verseId) {
       })
       .then(response => response.json())
       .then(data => { 
-        console.log('data: ', data);
-        console.log('verse: ', data.data.content);
-        var verse = data.data.content
-        console.log(verse);
-        document.querySelector("div").innerHTML = verse;
+        var verse = data.data.content;
+        var reference = data.data.reference;
+        document.querySelector("div").innerHTML = verse + reference; // <---- Sheri this is the gold!
       })
       .catch(error => console.error('Error:', error));
 }
@@ -38,17 +37,15 @@ function fetchVerses(chapterId) {
       })
       .then(response => response.json())
       .then(data => { 
-        console.log('data: ', data);
-        console.log('verse: ', data.data.content);
         var verse = data.data[Math.floor(Math.random()*data.data.length)];
-        console.log(verse);
         fetchVerse(verse.id);
       })
       .catch(error => console.error('Error:', error));
 }
 
-
-fetch(allChapters, {
+function fetchBooks(chapterBookId) {
+    var allChapters = `https://api.scripture.api.bible/v1/bibles/${bibleId}/books/${chapterBookId}/chapters`;
+    fetch(allChapters, {
     headers: {
       'api-key': 'e0069271426c93138b3100997ccfbd51',
     //   'mode': 'no-cors'
@@ -56,16 +53,25 @@ fetch(allChapters, {
   })
   .then(response => response.json())
   .then(data => { 
-    console.log('data: ', data);
-    console.log('verse: ', data.data.content);
     data.data.shift()
-    console.log("shift",data.data)
     var chapter = data.data[Math.floor(Math.random()*data.data.length)];
-    console.log(chapter);
     fetchVerses(chapter.id);
   })
   .catch(error => console.error('Error:', error));
+}
 
+fetch(allBooks, {
+    headers: {
+      'api-key': 'e0069271426c93138b3100997ccfbd51',
+    //   'mode': 'no-cors'
+    }
+  })
+  .then(response => response.json())
+  .then(data => { 
+    var books = data.data[Math.floor(Math.random()*data.data.length)];
+    fetchBooks(books.id);
+  })
+  .catch(error => console.error('Error:', error));
   
 fetch(pixaUrl)
   .then(response => response.json())
