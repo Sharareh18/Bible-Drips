eb59fdb3ef1a2d95a95f96a30de2c805a708adda
Sharareh Golshani <137013883+Sharareh18@users.noreply.github.com>
2023-08-14 11:24:49 -0700

    adding updated test files
    updated js, html and css files for testing image api

---
+++ Test/js/script.js
@@ -0,0 +1,130 @@
+var allBooks = 'https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/books';
+var bibleId = 'de4e12af7f28f599-02';
+var pixaUrl = `https://pixabay.com/api/?key=${pixaKey}`;
+
+var versePlacement = document.getElementById("insertVerseHere");
+var imagePlacement = document.getElementById("insertImageHere");
+var verseButton = document.getElementById("verseButton");
+var imageButton = document.getElementById("imageButton");
+var viewedVersesList = [];
+var viewedImagesList = [];
+var verse = [];
+var image = [];
+
+// var pixaKeyS = "38768763-bb697d80fc015bc0e4d2af0a5";
+var pixaKey = "38708546-2cfb7c47338a280cf45ea2b47";
+var bibleKey = "e0069271426c93138b3100997ccfbd51";
+
+
+
+// var displayVerse = function () {
+//   fetch(allBooks, {
+//     headers: {
+//       'api-key': 'e0069271426c93138b3100997ccfbd51',
+//       //   'mode': 'no-cors'
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       var books = data.data[Math.floor(Math.random() * data.data.length)];
+//       fetchBooks(books.id);
+//     })
+//     .catch(error => console.error('Error:', error));
+// };
+
+// function fetchVerse(verseId) {
+//   var singleVerse = `https://api.scripture.api.bible/v1/bibles/${bibleId}/verses/${verseId}?content-type=text&include-verse-numbers=false`;
+//   fetch(singleVerse, {
+//     headers: {
+//       'api-key': 'e0069271426c93138b3100997ccfbd51',
+//       //   'mode': 'no-cors'
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       var verse = data.data.content;
+//       var reference = data.data.reference;
+//       document.querySelector("#insertVerseHere").innerHTML = verse + reference; // <---- Sheri this is the gold!
+//     })
+//     .catch(error => console.error('Error:', error));
+// }
+
+// function fetchVerses(chapterId) {
+//   var allVerses = `https://api.scripture.api.bible/v1/bibles/${bibleId}/chapters/${chapterId}/verses`;
+//   fetch(allVerses, {
+//     headers: {
+//       'api-key': 'e0069271426c93138b3100997ccfbd51',
+//       //   'mode': 'no-cors'
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       var verse = data.data[Math.floor(Math.random() * data.data.length)];
+//       fetchVerse(verse.id);
+//     })
+//     .catch(error => console.error('Error:', error));
+// }
+
+// function fetchBooks(chapterBookId) {
+//   var allChapters = `https://api.scripture.api.bible/v1/bibles/${bibleId}/books/${chapterBookId}/chapters`;
+//   fetch(allChapters, {
+//     headers: {
+//       'api-key': 'e0069271426c93138b3100997ccfbd51',
+//       //   'mode': 'no-cors'
+//     }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       data.data.shift()
+//       var chapter = data.data[Math.floor(Math.random() * data.data.length)];
+//       fetchVerses(chapter.id);
+//     })
+//     .catch(error => console.error('Error:', error));
+// }
+
+
+
+
+ 
+function displayImage() {
+  fetch(`https://pixabay.com/api/?key=${pixaKey}&q=sky+clouds&image_type=photo&min_width=600&category=backgrounds`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('data', data);
+      var hitsNumber = Math.round(Math.random() * (20 - 1) +1);
+      var largeImageURL = data.hits[hitsNumber].largeImageURL;
+      localStorage.setItem("viewedImagesList", JSON.stringify(largeImageURL));
+      var img = "<img src='" + largeImageURL + "'width=400/>";
+      document.getElementById("insertImageHere").innerHTML = img;
+      // .catch(error => console.error('Error:', error));
+
+    });
+
+};
+
+
+// var showSearches = function () {
+//   var viewedVersesList = JSON.parse(localStorage.getItem("viewedVersesList"));
+//   if (viewedVersesList) {
+//     viewedVerses = viewedVersesList;
+//     for (i = 0; i < viewedVersesList.length; i++) {
+//       var viewedVerses = document.getElementById("viewedVersesList");
+//       viewedVerses.innerHTML = "<li>" + viewedVerses + "</li>";
+
+//     }
+//   }
+  // var viewedImagesList = JSON.parse(localStorage.getItem("viewedImagesList"));
+  // if (viewedImagesList) {
+  //   viewedImages = viewedImagesList;
+  //   for (i = 0; i < viewedImagesList.length; i++) {
+  //     var viewedImages = document.getElementById("viewedImagesList");
+  //     viewedImages.innerHTML = "<li>" + viewedImages + "</li>";
+
+  //   }
+  // };
+
+// // event listeners for user action on page 
+// verseButton.addEventListener("click", displayVerse);
+imageButton.addEventListener("click", displayImage);
